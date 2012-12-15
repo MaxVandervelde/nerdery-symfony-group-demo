15ae1e17bf8e686814fb4df6b1bedbbb71c619b6
Maxwell Vandervelde <Max@MaxVandervelde.com>
2012-12-15 13:48:44 -0600

    Adding symfony

---
+++ web/app_dev.php
@@ -0,0 +1,22 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+
+// If you don't want to setup permissions the proper way, just uncomment the following PHP line
+// read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
+//umask(0000);
+
+if (getenv('APPLICATION_ENV') !== 'dev') {
+    header('HTTP/1.0 403 Forbidden');
+    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+}
+
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+require_once __DIR__.'/../app/AppKernel.php';
+
+$kernel = new AppKernel('dev', true);
+$kernel->loadClassCache();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
