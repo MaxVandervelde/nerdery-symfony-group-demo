87c75d82c7ec51d6923b20394994bdd74a16a508
Maxwell Vandervelde <Max@MaxVandervelde.com>
2013-01-05 20:56:12 -0600

    Adding demo test
    Added a demo entity test for unit testing doctrine entities

---
+++ src/Nerdery/GroupsBundle/Tests/Entity/ClubsTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * ClubsTest.php
+ *
+ * @category NerderyGroupsBundle
+ * @package Tests
+ * @subpackage Controller
+ * @author Maxwell Vandervelde <Maxwell.Vandervelde@nerdery.com>
+ * @license http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode
+ *          Attribution-NonCommercial-ShareAlike 3.0 Unported
+ *          Some Rights Reserved
+ */
+
+namespace Nerdery\GroupsBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * ClubsTest
+ *
+ * @category NerderyGroupsBundle
+ * @package Tests
+ * @subpackage Controller
+ * @author Maxwell Vandervelde <Max@MaxVandervelde.com>
+ */
+class ClubsTest extends WebTestCase
+{
+    /**
+     * testClubs
+     *
+     * @return void
+     */
+    public function testClubs()
+    {
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+}
