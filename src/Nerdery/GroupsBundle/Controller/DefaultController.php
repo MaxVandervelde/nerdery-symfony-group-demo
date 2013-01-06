ea1aa253b8bd7f109ecad7e82964103180e5b6cf
Maxwell Vandervelde <Max@MaxVandervelde.com>
2013-01-05 19:24:15 -0600

    Deleted Acme bundle
    Deleted the acme bundle that comes with symfony2
    added in comments to defaultcontroller

--- src/Nerdery/GroupsBundle/Controller/DefaultController.php
+++ src/Nerdery/GroupsBundle/Controller/DefaultController.php
@@ -1,16 +1,42 @@
 <?php
+/**
+ * DefaultController
+ *
+ * @category NerderyGroupsBundle
+ * @package Controller
+ * @author Maxwell Vandervelde <Maxwell.Vandervelde@nerdery.com>
+ * @license http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode
+ *          Attribution-NonCommercial-ShareAlike 3.0 Unported
+ *          Some Rights Reserved
+ */
 
 namespace Nerdery\GroupsBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * DefaultController
+ *
+ * Base website controller landed on
+ *
+ * @category NerderyGroupsBundle
+ * @package Controller
+ * @author Maxwell Vandervelde <Maxwell.Vandervelde@nerdery.com>
+ */
 class DefaultController extends Controller
 {
     /**
+     * indexAction
+     *
+     * The default index page for the website
+     *
      * @Route("/hello/{name}")
      * @Template()
+     * @param string $name Demo string to echo
+     * @return array
      */
     public function indexAction($name)
     {
